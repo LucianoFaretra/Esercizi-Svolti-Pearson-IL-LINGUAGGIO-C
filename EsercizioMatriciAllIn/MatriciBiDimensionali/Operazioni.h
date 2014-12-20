8e6c03c613580bff55296bdfed6618e4bef93197
Luciano Faretra <LucianoFaretra@gmail.com>
2014-12-20 19:12:45 +0100

    Riordino

--- EsercizioMatriciAllIn/MatriciBiDimensionali/Operazioni.h
+++
@@ -1,4 +0,0 @@
-void MatriceMoltiplicazione(float matrice[][DIM], float matrice2[][DIM], float matrice3[][DIM]);
-void MatriceTrasposta(float matrice[][DIM], float matrice3[][DIM]);
-void MatriceSomma(float matrice[][DIM], float matrice2[][DIM], float matrice3[][DIM]);
-void ProdottoScalare(float matrice[][DIM], float matrice3[][DIM]);
