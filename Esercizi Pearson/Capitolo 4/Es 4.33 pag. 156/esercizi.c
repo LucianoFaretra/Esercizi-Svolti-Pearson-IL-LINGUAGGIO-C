6e17b4d6021c9cf9f606a0bbe6fac5087ddfae4a
Luciano Faretra <LucianoFaretra@gmail.com>
2014-12-30 16:53:48 +0100

    final

--- Esercizi Pearson/Capitolo 4/Es 4.33 pag. 156/esercizi.c
+++ Esercizi Pearson/Capitolo 4/Es 4.33 pag. 156/esercizi.c
@@ -28,86 +28,151 @@
 int main(void)
 {/*inizio funzione main*/
 
-    unsigned int contatoreNumeri = 0;
+    unsigned int contatoreNumeri = 1;
     unsigned int contatoreX;
     unsigned int i;
     unsigned int restoX;
-    char C = 'C';
-    char L = 'L';
-    char X = 'X';
-    char I = 'I';
-    char V = 'V';
 
 
     printf("%15s \t%14s", "Numeri Decimali", "Numeri Romani\n" );
     do{
         if(contatoreNumeri == 100){
-           printf("%-15d \t%-14c\n", contatoreNumeri, C);
+           printf("%-15d \t%-14c\n", contatoreNumeri, 'C');
+        }
+        else if(contatoreNumeri < 10){
+        printf("%-15d  \t", contatoreNumeri);
+                if(contatoreNumeri < 4){
+                    i = 1;
+                    while(i <= contatoreNumeri){
+                        printf("%c", 'I');
+                        i++;
+                    }
+                }
+                else if(contatoreNumeri == 4){
+                    i = 1;
+                    printf("%c", 'I');
+                    printf("%c", 'V');
+                }
+                else if(contatoreNumeri > 4 && contatoreNumeri <= 9){
+                    printf("%c", 'V');
+                    i = 1;
+                    while(i <= ( contatoreNumeri - 5)){
+                        printf("%c", 'I');
+                        i++;
+                    }
+                }
+                else if(contatoreNumeri == 9){
+                            printf("%c", 'I');
+                            printf("%c", 'X');
+                }
         }
         else if((contatoreX = contatoreNumeri / 10) >= 1){
             printf("%-15d  \t", contatoreNumeri);
 
-            if(contatoreX < 5){
-                if(((restoX = (contatoreNumeri % 10)) == 0)){
-                    i = 1;
-                    do{
-                        printf("%c", X);
-                        i++;
-                    }while(i <= contatoreX);
-                }
-
-                else{
-                    if(restoX < 4){
+                if(contatoreX < 5){
+                    if(((restoX = (contatoreNumeri % 10)) == 0)){
                         i = 1;
                         do{
-                            printf("%c", X);
+                            printf("%c", 'X');
                             i++;
                         }while(i <= contatoreX);
-                        i = 1;
-                        do{
-                            printf("%c", I);
-                            i++;
-                        }while(i <= restoX);
-                    }
-                }
-            }
-            else{
-                contatoreX -= 5;
-                printf("%c", 'L');
-                if(((restoX = ((contatoreNumeri -50) % 10)) == 0)){
-                    i = 1;
-                    while(i <= contatoreX){
-                        printf("%c", X);
-                        i++;
-                    }
-                }
-
-                else{
-                    if(restoX < 4){
-                        i = 1;
-                        while(i <= contatoreX){
-                            printf("%c", X);
-                            i++;
-                        }
-                        i = 1;
-                        while(i <= restoX){
-                            printf("%c", I);
-                            i++;
-                        }
                     }
                     else{
-
+                        if(restoX < 4){
+                            i = 1;
+                            do{
+                                printf("%c", 'X');
+                                i++;
+                            }while(i <= contatoreX);
+                            i = 1;
+                            do{
+                                printf("%c", 'I');
+                                i++;
+                            }while(i <= restoX);
+                        }
+                        else if(restoX == 4){
+                            i = 1;
+                            while(i <= contatoreX){
+                                printf("%c", 'X');
+                                i++;
+                            }
+                            printf("%c", 'I');
+                            printf("%c", 'V');
+                        }
+                        else if(restoX > 4 && restoX <= 9){
+                            i = 1;
+                            while(i <= contatoreX){
+                                printf("%c", 'X');
+                                i++;
+                            }
+                            printf("%c", 'V');
+                            i = 1;
+                            while(i <= ( restoX - 5)){
+                                printf("%c", 'I');
+                                i++;
+                            }
+                        }
                     }
                 }
+                else{
+                    contatoreX -= 5;
+                    printf("%c", 'L');
+                    if(((restoX = ((contatoreNumeri -50) % 10)) == 0)){
+                        i = 1;
+                        while(i <= contatoreX){
+                            printf("%c", 'X');
+                            i++;
+                        }
+                    }
 
-            }
-
-
-            printf("\n");
-
+                    else{
+                        if(restoX < 4){
+                            i = 1;
+                            while(i <= contatoreX){
+                                printf("%c", 'X');
+                                i++;
+                            }
+                            i = 1;
+                            while(i <= restoX){
+                                printf("%c", 'I');
+                                i++;
+                            }
+                        }
+                        else if(restoX == 4){
+                            i = 1;
+                            while(i <= contatoreX){
+                                printf("%c", 'X');
+                                i++;
+                            }
+                            printf("%c", 'I');
+                            printf("%c", 'V');
+                        }
+                        else if(restoX > 4 && restoX <= 9){
+                            i = 1;
+                            while(i <= contatoreX){
+                                printf("%c", 'X');
+                                i++;
+                            }
+                            if(restoX == 4){
+                            printf("%c", 'I');
+                            }
+                            printf("%c", 'V');
+                            i = 1;
+                            while(i <= ( restoX - 5)){
+                                printf("%c", 'I');
+                                i++;
+                            }
+                        }
+                        else if(restoX == 9){
+                            printf("%c", 'I');
+                            printf("%c", 'X');
+                        }
+                }
 
         }
 
+ }
+    printf("\n");
     contatoreNumeri++;
     }while( contatoreNumeri <= (unsigned int)MAXNUMERI );
 
