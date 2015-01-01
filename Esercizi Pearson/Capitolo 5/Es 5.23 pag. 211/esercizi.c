fbdae99d8554955bb6d2a449410a02d77fa8ef3c
Luciano Faretra <LucianoFaretra@gmail.com>
2015-01-01 12:03:12 +0100

    Ultimato

--- Esercizi Pearson/Capitolo 5/Es 5.23 pag. 211/esercizi.c
+++ Esercizi Pearson/Capitolo 5/Es 5.23 pag. 211/esercizi.c
@@ -10,7 +10,7 @@
     secondiTempoInput               secondi su cui calcolare il tempo (quadrante 12h)               unsigned int >= 0, <= 59
 
     OUTPUT                          DESCRIZIONE                                                     DOMIONIO
-    secondiInseriti                 pari a secondiTempoInput +  minutiTempoInput * 60 +             unsigned int >= 0
+    secondiOrario                 pari a secondiTempoInput +  minutiTempoInput * 60 +             unsigned int >= 0
                                     oreTempoInput * 60 * 60
 
 
@@ -26,8 +26,11 @@
        ******************************************************************************************
        Funzione Main
 
-        oreInSecondi()
+        oreInSecondi(oreTempoInput, minutiTempoInput, secondiTempoInput)
 
+        secondiOrario = secondiTempoInput + ( minutiTempoInput + ( oreTempoInput * 60 ) ) * 60
+
+        ritorna secondiOrario
 
 
         ******************************************************************************************
@@ -94,83 +97,94 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
 
 int main(void)
 {//inizio funzione main
 
-    int quozienteDueNumeri( int numeroA, int numeroB );
-    int restoQuozienteDueNumeri( int numeroA, int numeroB );
-    void numeroIntervallato( int numeroA );
+    unsigned int oreInSecondi( unsigned int oreTempoInput, unsigned int minutiTempoInput, unsigned int secondiTempoInput);
 
-    int numeroA;
-    int numeroB;
+    unsigned int oreTempoInput;
+    unsigned int minutiTempoInput;
+    unsigned int secondiTempoInput;
+    unsigned int secondiPrimoOrario;
+    unsigned int secondiSecondoOrario;
+    unsigned int valoreSentinella = 0;
 
+    puts( "Inserisci il Primo Orario: " );
     do{
-        printf( "%s", "Inserire il primo numero: ");
-        scanf( "%d", &numeroA);
-        printf( "%s", "Inserire il numero con cui dividere il primo numero: ");
-        scanf( "%d", &numeroB);
-
-        if( numeroA > 0 ){
-            if( numeroB > 0 ){
-                printf( "Il quoziente di %d diviso %d e' pari a: %d\n", numeroA, numeroB, quozienteDueNumeri( numeroA, numeroB ) );
-                printf( "Il resto di %d diviso %d e' pari a: %d\n", numeroA, numeroB, restoQuozienteDueNumeri( numeroA, numeroB ) );
-                printf( "Il numero %d intervallato di 2 spazi per ogni cifra e' pari a: ", numeroA);
-                numeroIntervallato( numeroA );
+        printf( "%s", "Inserire le ore che compongono l'orario ( quadrante 12h ): " );
+        scanf( "%u", &oreTempoInput );
+        if( oreTempoInput >= 0 && oreTempoInput <= 11 ){
+            printf( "%s", "Inserire i minuti che compongono l'orario: ");
+            scanf( "%u", &minutiTempoInput );
+            if( minutiTempoInput >= 0 && minutiTempoInput <= 59 ){
+                printf( "%s", "Inserire i secondiTempoInput che compongono l'orario: " );
+                scanf( "%u", &secondiTempoInput );
+                if( secondiTempoInput >= 0 && secondiTempoInput <= 59 ){
+                    valoreSentinella = 1;
+                }
+                else{
+                    puts( "Attenzione non e' stato inserito un valore in secondi valido." );
+                }
             }
             else{
-                    puts("E' stata tentata una divisione per 0 non consentita");
+                puts( "Attenzione non e' stato inserito un valore in minuti valido." );
             }
         }
         else{
-            puts("Non e' stato inserito un valore valido ripetere l'inserimento");
+             puts( "Attenzione non e' stato inserito un valore in ore valido." );
         }
-    }while( numeroA <= 0 && numeroA <= 0 );
+
+    }while( valoreSentinella != 1 );
+
+    secondiPrimoOrario = oreInSecondi( oreTempoInput, minutiTempoInput, secondiTempoInput );
+
+    puts( "Secondo Orario:" );
+    do{
+        printf( "%s", "Inserire le ore che compongono l'orario ( quadrante 12h ): " );
+        scanf( "%u", &oreTempoInput );
+        if( oreTempoInput >= 0 && oreTempoInput <= 11 ){
+            printf( "%s", "Inserire i minuti che compongono l'orario: ");
+            scanf( "%u", &minutiTempoInput );
+            if( minutiTempoInput >= 0 && minutiTempoInput <= 59 ){
+                printf( "%s", "Inserire i secondiTempoInput che compongono l'orario: " );
+                scanf( "%u", &secondiTempoInput );
+                if( secondiTempoInput >= 0 && secondiTempoInput <= 59 ){
+                    valoreSentinella = 1;
+                }
+                else{
+                    puts( "Attenzione non e' stato inserito un valore in secondi valido." );
+                }
+            }
+            else{
+                puts( "Attenzione non e' stato inserito un valore in minuti valido." );
+            }
+        }
+        else{
+             puts( "Attenzione non e' stato inserito un valore in ore valido." );
+        }
+
+    }while( valoreSentinella != 1 );
+
+    secondiSecondoOrario = oreInSecondi( oreTempoInput, minutiTempoInput, secondiTempoInput );
+
+    printf( "%s", "La differenza in secondi tra i due orari e' di: ");
+        if( secondiPrimoOrario >= secondiSecondoOrario ){
+            printf( "%d", secondiPrimoOrario - secondiSecondoOrario );
+        }
+        else{
+            printf( "%d", secondiSecondoOrario - secondiPrimoOrario );
+        }
+
 
 return 0;
 }//Fine funzione main
 
-int quozienteDueNumeri( int numeroA, int numeroB )
+unsigned int oreInSecondi( unsigned int oreTempoInput, unsigned int minutiTempoInput, unsigned int secondiTempoInput)
 {
+    unsigned int secondiOrario;
 
-return floor( numeroA / numeroB );
+    secondiOrario = ( secondiTempoInput + ( minutiTempoInput + ( oreTempoInput * 60 ) ) * 60 );
+
+return secondiOrario;
 }
-
-int restoQuozienteDueNumeri( int numeroA, int numeroB )
-{
-
-return ( numeroA % numeroB );
-}
-
-void numeroIntervallato( int numeroA )
-{
-    int quoziente;
-
-    if( numeroA >= 10000 ){
-        printf( "%d  ", quoziente = quozienteDueNumeri( numeroA, 10000 ) );
-        numeroA -= quoziente * 10000;
-        numeroIntervallato( numeroA );
-    }
-    else if( numeroA >= 1000 ){
-        printf( "%d  ", quoziente = quozienteDueNumeri( numeroA, 1000 ) );
-        numeroA -= quoziente * 1000;
-        numeroIntervallato( numeroA );
-    }
-    else if( numeroA >= 100 ){
-        printf( "%d  ", quoziente = quozienteDueNumeri( numeroA, 100 ) );
-        numeroA -= quoziente * 100;
-        numeroIntervallato( numeroA );
-    }
-    else if( numeroA >= 10 ){
-        printf( "%d  ", quoziente = quozienteDueNumeri( numeroA, 10 ) );
-        numeroA -= quoziente * 10;
-        numeroIntervallato( numeroA );
-    }
-    else{
-        printf( "%d  ", numeroA);
-    }
-
-return;
-}
-
